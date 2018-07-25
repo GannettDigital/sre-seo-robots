ae3d261c5613c012fb53aa69cdb98aa59fa8ce57
Shreyas Gune <sgune@gannett.com>
2018-07-25 14:30:02 -0400

    PENG-4459 make seo-robots checks great again

---
+++ roboget.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+arr=('alamogordonews' 'app' 'argusleader' 'azcentral' 'battlecreekenquirer' 'baxterbulletin' 'blackmountainnews' 'bucyrustelegraphforum' 'burlingtonfreepress' 'caller' 'centralfloridafuture' 'chillicothegazette' 'cincinnati' 'citizen-times' 'clarionledger' 'coloradoan' 'commercialappeal' 'coshoctontribune' 'courier-journal' 'courierpostonline' 'courierpress' 'currentargus' 'daily-times' 'dailyrecord' 'dailyworld' 'delawarebeaches' 'delawareonline' 'delmarvanow' 'demingheadlight' 'democratandchronicle' 'desertsun' 'desmoinesregister' 'dmjuice' 'dnj' 'elpasotimes' 'elpasoymas' 'elsoldesalinas' 'eveningsun' 'farmersadvance' 'fdlreporter' 'flipsidepa' 'floridatoday' 'freep' 'fsunews' 'gametimepa' 'gosanangelo' 'greatfallstribune' 'greenbaypressgazette' 'greenvilleonline' 'guampdn' 'hattiesburgamerican' 'hawkcentral' 'hometownlife' 'htrnews' 'independentmail' 'indystar' 'inyork' 'ithacajournal' 'jacksonsun' 'jconline' 'jsonline' 'kitsapsun' 'knoxnews' 'lancastereaglegazette' 'lansingstatejournal' 'lavozarizona' 'lcsun-news' 'ldnews' 'livingstondaily' 'lohud' 'mansfieldnewsjournal' 'marconews' 'marionstar' 'marshfieldnewsherald' 'metroparentmagazine' 'montgomeryadvertiser' 'mycentraljersey' 'naplesnews' 'newarkadvocate' 'news-leader' 'news-press' 'newsleader' 'northjersey' 'packersnews' 'pal-item' 'pnj' 'portclintonnewsherald' 'postcrescent' 'poughkeepsiejournal' 'press-citizen' 'pressconnects' 'publicopiniononline' 'redding' 'reno' 'reporternews' 'rgj' 'ruidosonews' 'scsun-news' 'sctimes' 'sheboyganpress' 'shreveporttimes' 'stargazette' 'statesmanjournal' 'stevenspointjournal' 'tallahassee' 'tcpalm' 'tennessean' 'theadvertiser' 'thecalifornian' 'thedailyjournal' 'thegleaner' 'thehammontonnews' 'theleafchronicle' 'thenews-messenger' 'thenewsstar' 'thenorthwestern' 'thespectrum' 'thestarpress' 'thetimesherald' 'thetowntalk' 'timesrecordnews' 'trainuscp' 'upstateparent' 'vcstar' 'visaliatimesdelta' 'wausaudailyherald' 'wisconsinrapidstribune' 'wisfarmer' 'ydr' 'yorkdispatch' 'zanesvilletimesrecorder')
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+  echo "processing $i"
+  curl "https://www.$i.com/robots.txt" >> "$i.txt"
+  echo "$i is DONE \n ----------\n"
+   # or do whatever with individual element of the array
+done
